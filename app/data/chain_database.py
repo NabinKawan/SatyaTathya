6d2e1b484be5407dcbbf30f82f3d8186a8faf4e7
NibanK <nabin.kawan.9@gmail.com>
2023-01-30 13:55:55 +0545

    Add redis db and Remove blockchain folder

--- app/data/chain_database.py
+++ app/data/chain_database.py
@@ -1,15 +1,18 @@
-from app.models.dtos.block_dto import BlockDto
-from typing import List
+import redis
 
+redis_db = redis.Redis(
+    host='127.0.0.1',
+    port=6379)
 
-class ChainDatabase(object):
-    """
-    Singleton Class
-    """
-    chain_datas: List[BlockDto] = []
+# class ChainDatabase(object):
+#     """
+#     Singleton Class
+#     """
+#     chain_datas: List[BlockDto] = []
 
-    def __new__(cls, *args, **kwargs) -> None:
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(ChainDatabase, cls).__new__(
-                cls, *args, **kwargs)
-        return cls.instance
+#     def __new__(cls, *args, **kwargs) -> None:
+#         if not hasattr(cls, 'instance'):
+#             cls.instance = super(ChainDatabase, cls).__new__(
+#                 cls, *args, **kwargs)
+#         return cls.instance
+
--- app/services/impl/local_db_service.py
+++ app/services/impl/local_db_service.py
@@ -1,24 +1,27 @@
-from app.data.chain_database import ChainDatabase
+from app.data.chain_database import redis_db
 from app.models.dtos.block_dto import BlockDto
 from app.models.enums.data_format_enum import DataFormatEnum
 from app.services.db_service import DbService
+import json
 
-db = ChainDatabase()
+db = redis_db
 
 
 class LocalDbService(DbService):
 
     def add_data(self, block: BlockDto):
-        db.chain_datas.append(block)
+        db.set(block.block_hash,json.dumps(block.json()))
 
     def get_prev_hash(self):
-        return db.chain_datas[-1].block_hash
+        pass
+        # return db.set[-1].block_hash
 
     def get_all_datas(self, data_type: DataFormatEnum = DataFormatEnum.BLOCK):
-        if data_type == DataFormatEnum.BLOCK:
-            return db.chain_datas
-        elif data_type == DataFormatEnum.JSON:
-            json_datas = []
-            for data in db.chain_datas:
-                json_datas.append(data.dict())
-            return json_datas
+        pass
+        # if data_type == DataFormatEnum.BLOCK:
+        #     return db.chain_datas
+        # elif data_type == DataFormatEnum.JSON:
+        #     json_datas = []
+        #     for data in db.chain_datas:
+        #         json_datas.append(data.dict())
+        #     return json_datas
